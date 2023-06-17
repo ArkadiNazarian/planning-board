0174e46b699854390758fd5104e48de86add529c
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-06-17 09:39:44 +0430

    fix the border-radius

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
             release_train_list.map((train_value, train_index) => (
               <tr key={train_value._id}>
                 <th className={`${train_index === 0 ? "tw-border-none" : "tw-border"}`}>
-                  <div className={`${(train_index === 1 || train_index % 4 === 1) ? "tw-bg-custom_yellow tw-text-custom_yellow" : (train_index === 2 || train_index % 4 === 2) ? "tw-bg-custom_red tw-text-custom_red" : (train_index === 3 || train_index % 4 === 3) ? "tw-bg-custom_purple tw-text-custom_purple" : (train_index === 4 || (train_index % 4 === 0 && train_index !== 0)) && "tw-bg-custom_pink tw-text-custom_pink"} tw-p-2 tw-min-w-max`}>
+                  <div className={`${(train_index === 1 || train_index % 4 === 1) ? "tw-bg-custom_yellow tw-text-custom_yellow" : (train_index === 2 || train_index % 4 === 2) ? "tw-bg-custom_red tw-text-custom_red" : (train_index === 3 || train_index % 4 === 3) ? "tw-bg-custom_purple tw-text-custom_purple" : (train_index === 4 || (train_index % 4 === 0 && train_index !== 0)) && "tw-bg-custom_pink tw-text-custom_pink"} tw-p-2 tw-rounded-lg tw-min-w-max`}>
                     {train_value.title}
                   </div>
                 </th>
